18d77303c6945fcfaff59c3052562fe95731327f
Mitch Bishop <Mitch.Bishop@NGIC.COM>
2023-06-29 11:29:09 -0400

    Mostly adding rabbitmq tutorial.

---
+++ Blazor1/Testing/Class.cs
@@ -0,0 +1,21 @@
+﻿namespace Blazor1.Testing
+{
+    public class Class
+    {
+        #region Private Fields
+
+        #endregion Private Fields
+
+        #region Public Constructors
+
+        #endregion Public Constructors
+
+        #region Private Constructors
+
+        #endregion Private Constructors
+
+        #region Public Properties
+
+        #endregion Public Properties
+    }
+}
